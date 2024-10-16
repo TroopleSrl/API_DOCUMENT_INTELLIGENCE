6aea0df8cbb4526dcdf2d554c2c951279150034e
Mark Shevchenko <mark2004kyki@gmail.com>
2024-10-16 16:16:28 +0200

    merged with lioche + added image check

--- app/tasks/handle_document.py
+++ app/tasks/handle_document.py
@@ -7,9 +7,8 @@ def handle_document(document: bytes, type: str, mime: str):
         print(f"Handling document of type {type} with MIME type {mime}")
         parser = get_parser(type)(document)
         text = parser.parse(mime)
+        del parser
         return text
     except Exception as e:
         print(f"Failed to handle document of type {type} with MIME type {mime}")
         print(e)
-    finally:
-        ...
